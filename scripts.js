e0e9cd3d51fe5eac9198866ab265221a65adfa9b
DantheSE <dan.doyin@gmail.com>
2024-05-16 12:53:23 +0100

    fixed problem with player and computer logic for each round

--- scripts.js
+++ scripts.js
@@ -1,17 +1,16 @@
 //find a way to collect user input and determine if it's the user or computer who has logged on,
 // give variables accordingly and change the name at the start based on who logs on
 
-const ROCK = "computer chose rock";
-const PAPER = "computer chose paper";
-const SCISSORS = "computer chose scissors";
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
 
-function randomNumber() {
+const randomValue = () => {
   let random = Math.floor(Math.random() * 3);
   return random;
-}
+};
 
-var randomValue = randomNumber();
-// console.log(randomValue);
+// console.log(randomValue());
 
 // function choiceMaker() {
 //   if (randomValue === 0) {
@@ -28,37 +27,74 @@ var randomValue = randomNumber();
 // console.log(choiceMaker());
 
 function getComputerChoice() {
-  if (randomValue === 0) {
+  switch (randomValue()) {
+    case 0: {
+      return ROCK;
+    }
+    case 1: {
+      return PAPER;
+    }
+    case 2: {
+      return SCISSORS;
+    }
+  }
+}
+
+const USER_ROCK = "rock";
+const USER_PAPER = "paper";
+const USER_SCISSORS = "scissors";
+
+function getHumanChoice() {
+  let userInputText = prompt("What's your choice? Rock, paper or scissors");
+  let finalUserInput = userInputText.toLowerCase();
+  //   let userInputNumeric = +userInputText;
+  if (finalUserInput === "rock") {
     return ROCK;
-  } else if (randomValue === 1) {
+  } else if (finalUserInput === "paper") {
     return PAPER;
-  } else if (randomValue === 2) {
+  } else if (finalUserInput === "scissors") {
     return SCISSORS;
   } else {
     console.log("error");
   }
 }
-console.log(getComputerChoice());
-
-const USER_ROCK = "user chose rock";
-const USER_PAPER = "user chose paper";
-const USER_SCISSORS = "user chose scissors";
-
-function getHumanChoice() {
-  let userInputText = prompt("What's your choice? Rock, paper or scissors");
-  let finalUserInput = userInputText.toLocaleLowerCase();
-  //   let userInputNumeric = +userInputText;
-  if (finalUserInput === "rock") {
-    return USER_ROCK;
-  } else if (finalUserInput === "paper") {
-    return USER_PAPER;
-  } else if (finalUserInput === "scissors") {
-    return USER_SCISSORS;
-  } else {
-    console.log("error");
-  }
-}
-console.log(getHumanChoice());
 
 var humanScore = 0;
 var computerScore = 0;
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+console.log(humanChoice);
+console.log(computerChoice);
+playRound(humanChoice, computerChoice);
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log("You lose! Paper beats rock");
+    computerScore++;
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log("You win! Paper beats rock");
+    humanScore++;
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    console.log("You lose! Scissors beats paper");
+    computerScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log("You win! Scissors beats paper");
+    humanScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    console.log("You lose! Rock beats scissors");
+    computerScore++;
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log("You win! Rock beats scissors");
+    humanScore++;
+  } else if (
+    (humanChoice === "rock" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "paper")
+  ) {
+    console.log("tie");
+  }
+}
+
+function playGame() {}
